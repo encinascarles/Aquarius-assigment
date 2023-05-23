587aeb6ec6f32afd10fde8a73c6c116cd059bb68
Carles Encinas <thecrax@ganxo.net>
2023-05-23 13:55:47 +0200

    implemented all 3 buttons and added pointer cursor

--- about-us.js
+++ about-us.js
@@ -1,15 +1,27 @@
-document.getElementById("Marca").addEventListener("click", changeToMarca);
+document.getElementById("marca").addEventListener("click", changeToMarca);
+document.getElementById("siguenos").addEventListener("click", changeToSiguenos);
+document.getElementById("conoce").addEventListener("click", changeToConoce);
 
 
+function changeToSiguenos(){
+    const listItem = document.querySelector(".about-us .content");
+    const newItem = document.createElement('div');
+    newItem.classList.add('content');
+    newItem.innerHTML = '<p>PAGE 1 (CONTENT)</p>';
+    listItem.parentNode.replaceChild(newItem, listItem);
+}
 function changeToMarca(){
     const listItem = document.querySelector(".about-us .content");
-
-
-
     const newItem = document.createElement('div');
     newItem.classList.add('content');
     newItem.innerHTML = '<p>PAGE 2 (CONTENT)</p>';
-    
+    listItem.parentNode.replaceChild(newItem, listItem);
+}
+function changeToConoce(){
+    const listItem = document.querySelector(".about-us .content");
+    const newItem = document.createElement('div');
+    newItem.classList.add('content');
+    newItem.innerHTML = '<p>PAGE 3 (CONTENT)</p>';
     listItem.parentNode.replaceChild(newItem, listItem);
 }
 
