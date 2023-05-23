aeaa928f08fec20622a049d82a3ed1c3104fb912
Carles Encinas <thecrax@ganxo.net>
2023-05-23 18:08:18 +0200

    "Siguenos" page updated

--- about-us.js
+++ about-us.js
@@ -2,48 +2,50 @@ document.getElementById("siguenos").addEventListener("click", changeToSiguenos);
 document.getElementById("marca").addEventListener("click", changeToMarca);
 document.getElementById("conoce").addEventListener("click", changeToConoce);
 
+function changeToSiguenos() {
+  const listItem = document.querySelector(".about-us .content");
+  const newItem = document.createElement("div");
+  newItem.classList.add("content");
+  newItem.innerHTML = `<div id="siguenos-content">
+    <img src="resources/images/Facebook_100x100.png">
+    <img src="resources/images/Twitter_100x100.png">
+    <img src="resources/images/YouTube_100x100.png">
+    <img src="resources/images/Instagram_100x100.png">
+  </div>`;
+  listItem.parentNode.replaceChild(newItem, listItem);
 
-function changeToSiguenos(){
-    const listItem = document.querySelector(".about-us .content");
-    const newItem = document.createElement('div');
-    newItem.classList.add('content');
-    newItem.innerHTML = '<p>PAGE 1 (CONTENT)</p>';
-    listItem.parentNode.replaceChild(newItem, listItem);
-
-    document.getElementById("siguenos").style.borderBottom='4px solid red';
-    document.getElementById("marca").style.borderBottom='none';
-    document.getElementById("conoce").style.borderBottom='none';
-    document.getElementById("siguenos").style.fontWeight='bold';
-    document.getElementById("marca").style.fontWeight='normal';
-    document.getElementById("conoce").style.fontWeight='normal';
+  document.getElementById("siguenos").style.borderBottom = "4px solid red";
+  document.getElementById("marca").style.borderBottom = "none";
+  document.getElementById("conoce").style.borderBottom = "none";
+  document.getElementById("siguenos").style.fontWeight = "bold";
+  document.getElementById("marca").style.fontWeight = "normal";
+  document.getElementById("conoce").style.fontWeight = "normal";
 }
-function changeToMarca(){
-    const listItem = document.querySelector(".about-us .content");
-    const newItem = document.createElement('div');
-    newItem.classList.add('content');
-    newItem.innerHTML = '<p>PAGE 2 (CONTENT)</p>';
-    listItem.parentNode.replaceChild(newItem, listItem);
+function changeToMarca() {
+  const listItem = document.querySelector(".about-us .content");
+  const newItem = document.createElement("div");
+  newItem.classList.add("content");
+  newItem.innerHTML = "<p>PAGE 2 (CONTENT)</p>";
+  listItem.parentNode.replaceChild(newItem, listItem);
 
-    document.getElementById("siguenos").style.borderBottom='none';
-    document.getElementById("marca").style.borderBottom='4px solid red';
-    document.getElementById("conoce").style.borderBottom='none';
-    document.getElementById("siguenos").style.fontWeight='normal';
-    document.getElementById("marca").style.fontWeight='bold';
-    document.getElementById("conoce").style.fontWeight='normal';
+  document.getElementById("siguenos").style.borderBottom = "none";
+  document.getElementById("marca").style.borderBottom = "4px solid red";
+  document.getElementById("conoce").style.borderBottom = "none";
+  document.getElementById("siguenos").style.fontWeight = "normal";
+  document.getElementById("marca").style.fontWeight = "bold";
+  document.getElementById("conoce").style.fontWeight = "normal";
 }
-function changeToConoce(){
-    const listItem = document.querySelector(".about-us .content");
-    const newItem = document.createElement('div');
-    newItem.classList.add('content');
-    newItem.innerHTML = '<p>PAGE 3 (CONTENT)</p>';
-    listItem.parentNode.replaceChild(newItem, listItem);
+function changeToConoce() {
+  const listItem = document.querySelector(".about-us .content");
+  const newItem = document.createElement("div");
+  newItem.classList.add("content");
+  newItem.innerHTML = "<p>PAGE 3 (CONTENT)</p>";
+  listItem.parentNode.replaceChild(newItem, listItem);
 
-    document.getElementById("siguenos").style.borderBottom='none';
-    document.getElementById("marca").style.borderBottom='none';
-    document.getElementById("conoce").style.borderBottom='4px solid red';
-    document.getElementById("siguenos").style.fontWeight='normal';
-    document.getElementById("marca").style.fontWeight='normal';
-    document.getElementById("conoce").style.fontWeight='bold';
-    
+  document.getElementById("siguenos").style.borderBottom = "none";
+  document.getElementById("marca").style.borderBottom = "none";
+  document.getElementById("conoce").style.borderBottom = "4px solid red";
+  document.getElementById("siguenos").style.fontWeight = "normal";
+  document.getElementById("marca").style.fontWeight = "normal";
+  document.getElementById("conoce").style.fontWeight = "bold";
 }
-
