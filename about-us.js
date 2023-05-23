160b5aea35fa90cf1fa8046c2ad1edf3e229fddb
Carles Encinas <thecrax@ganxo.net>
2023-05-23 18:42:26 +0200

    "conoce a otros paises" page updated

--- about-us.js
+++ about-us.js
@@ -58,7 +58,129 @@ function changeToConoce() {
   const listItem = document.querySelector(".about-us .content");
   const newItem = document.createElement("div");
   newItem.classList.add("content");
-  newItem.innerHTML = "<p>PAGE 3 (CONTENT)</p>";
+  newItem.innerHTML = `<div id="conoce-content">
+  <div>
+    <h2>North America</h2>
+    <p>
+      Global<br />
+      Canada (English)<br />
+      Canada (French)
+    </p>
+  </div>
+  <div>
+    <h2>North America</h2>
+    <p>
+      Global<br />
+      Canada (English)<br />
+      Canada (French)
+    </p>
+  </div>
+  <div>
+    <h2>Latin America</h2>
+    <p>
+      Argentina<br />
+      Bolivia<br />
+      Brazil<br />
+      Chile<br />
+      Colombia<br />
+      Costa Rica<br />
+      Dominican Republic<br />
+      Ecuador<br />
+      El Salvador<br />
+      Guatemala<br />
+      Honduras<br />
+      Mexico<br />
+      Nicaragua<br />
+      Panama<br />
+      Paraguay<br />
+      Peru<br />
+      Uruguay<br />
+      Venezuela
+    </p>
+  </div>
+
+  <div>
+    <h2>Africa</h2>
+    <p>
+      Ivory Coast<br />
+      Kenya<br />
+      Nigeria<br />
+      South Africa
+    </p>
+  </div>
+
+  <div>
+    <h2>Europe</h2>
+    <p>
+      Austria<br />
+      Belgium and Luxembourg (Dutch)<br />
+      Belgium and Luxembourg (French)<br />
+      Bosnia and Herzegovina<br />
+      Bulgaria<br />
+      Croatia<br />
+      Cyprus<br />
+      Czech Republic<br />
+      Denmark<br />
+      Estonia<br />
+      Finland<br />
+      France<br />
+      Germany<br />
+      Great Britain<br />
+      Greece<br />
+      Hungary<br />
+      Iceland<br />
+      Ireland<br />
+      Italy<br />
+      Latvia<br />
+      Lithuania<br />
+      Netherlands<br />
+      North Macedonia<br />
+      Norway<br />
+      Poland<br />
+      Portugal<br />
+      Romania<br />
+      Russia<br />
+      Serbia<br />
+      Slovakia<br />
+      Slovenia<br />
+      Spain<br />
+      Sweden<br />
+      Switzerland (German)<br />
+      Switzerland (French)<br />
+      Ukraine
+    </p>
+  </div>
+
+  <div>
+    <h2>Eurasia</h2>
+    <p>
+      Middle East (Arabic)<br />
+      Middle East (English)<br />
+      Morocco<br />
+      Pakistan
+    </p>
+  </div>
+
+  <div>
+    <h2>Asia Pacific</h2>
+    <p>
+      Australia<br />
+      China<br />
+      China - Hong Kong<br />
+      India<br />
+      Indonesia (Bahasa Indonesia)<br />
+      Indonesia (English)<br />
+      Japan<br />
+      Malaysia (English)<br />
+      Malaysia (Malay)<br />
+      New Zealand<br />
+      Singapore<br />
+      South Korea<br />
+      Vietnam (English)<br />
+      Vietnam (Vietnamese)
+    </p>
+  </div>
+</div>`;
   listItem.parentNode.replaceChild(newItem, listItem);
 
   document.getElementById("siguenos").style.borderBottom = "none";
