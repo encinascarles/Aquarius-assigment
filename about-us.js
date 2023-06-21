3b09c1a409fe63f51c44c9821e3eb6390f8f4c9d
Pau Fernandez <pau.fernandez@upc.edu>
2023-06-21 12:20:42 +0200

    Generate World Countries from pure data

--- about-us.js
+++ about-us.js
@@ -60,133 +60,103 @@ function changeToMarca() {
   conoce.style.borderBottom = "none";
   conoce.style.fontWeight = "normal";
 }
+
+const world = {
+  "North America": ["Global", "Canada (English)", "Canada (French)"],
+  "Latin America": [
+    "Argentina",
+    "Bolivia",
+    "Brazil",
+    "Chile",
+    "Colombia",
+    "Costa Rica",
+    "Dominican Republic",
+    "Ecuador",
+    "El Salvador",
+    "Guatemala",
+    "Honduras",
+    "Mexico",
+    "Nicaragua",
+    "Panama",
+    "Paraguay",
+    "Peru",
+    "Uruguay",
+    "Venezuela",
+  ],
+  Africa: ["Ivory Coast", "Kenya", "Nigeria", "South Africa"],
+  Europe: [
+    "Austria",
+    "Belgium and Luxembourg (Dutch)",
+    "Belgium and Luxembourg (French)",
+    "Bosnia and Herzegovina",
+    "Bulgaria",
+    "Croatia",
+    "Cyprus",
+    "Czech Republic",
+    "Denmark",
+    "Estonia",
+    "Finland",
+    "France",
+    "Germany",
+    "Great Britain",
+    "Greece",
+    "Hungary",
+    "Iceland",
+    "Ireland",
+    "Italy",
+    "Latvia",
+    "Lithuania",
+    "Netherlands",
+    "North Macedonia",
+    "Norway",
+    "Poland",
+    "Portugal",
+    "Romania",
+    "Russia",
+    "Serbia",
+    "Slovakia",
+    "Slovenia",
+    "Spain",
+    "Sweden",
+    "Switzerland (German)",
+    "Switzerland (French)",
+    "Ukraine",
+  ],
+  Eurasia: [
+    "Middle East (Arabic)",
+    "Middle East (English)",
+    "Morocco",
+    "Pakistan",
+  ],
+  "Asia Pacific": [
+    "Australia",
+    "China",
+    "China - Hong Kong",
+    "India",
+    "Indonesia (Bahasa Indonesia)",
+    "Indonesia (English)",
+    "Japan",
+    "Malaysia (English)",
+    "Malaysia (Malay)",
+    "New Zealand",
+    "Singapore",
+    "South Korea",
+    "Vietnam (English)",
+    "Vietnam (Vietnamese)",
+  ],
+};
+
 function changeToConoce() {
   const listItem = document.querySelector(".about-us .content");
   const newItem = document.createElement("div");
   newItem.classList.add("content");
-  newItem.innerHTML = `<div id="conoce-content">
-  <div>
-    <h2>North America</h2>
-    <p>
-      Global<br />
-      Canada (English)<br />
-      Canada (French)
-    </p>
-  </div>
-  <div>
-    <h2>North America</h2>
-    <p>
-      Global<br />
-      Canada (English)<br />
-      Canada (French)
-    </p>
-  </div>
-  <div>
-    <h2>Latin America</h2>
-    <p>
-      Argentina<br />
-      Bolivia<br />
-      Brazil<br />
-      Chile<br />
-      Colombia<br />
-      Costa Rica<br />
-      Dominican Republic<br />
-      Ecuador<br />
-      El Salvador<br />
-      Guatemala<br />
-      Honduras<br />
-      Mexico<br />
-      Nicaragua<br />
-      Panama<br />
-      Paraguay<br />
-      Peru<br />
-      Uruguay<br />
-      Venezuela
-    </p>
-  </div>
-
-  <div>
-    <h2>Africa</h2>
-    <p>
-      Ivory Coast<br />
-      Kenya<br />
-      Nigeria<br />
-      South Africa
-    </p>
-  </div>
-
-  <div>
-    <h2>Europe</h2>
-    <p>
-      Austria<br />
-      Belgium and Luxembourg (Dutch)<br />
-      Belgium and Luxembourg (French)<br />
-      Bosnia and Herzegovina<br />
-      Bulgaria<br />
-      Croatia<br />
-      Cyprus<br />
-      Czech Republic<br />
-      Denmark<br />
-      Estonia<br />
-      Finland<br />
-      France<br />
-      Germany<br />
-      Great Britain<br />
-      Greece<br />
-      Hungary<br />
-      Iceland<br />
-      Ireland<br />
-      Italy<br />
-      Latvia<br />
-      Lithuania<br />
-      Netherlands<br />
-      North Macedonia<br />
-      Norway<br />
-      Poland<br />
-      Portugal<br />
-      Romania<br />
-      Russia<br />
-      Serbia<br />
-      Slovakia<br />
-      Slovenia<br />
-      Spain<br />
-      Sweden<br />
-      Switzerland (German)<br />
-      Switzerland (French)<br />
-      Ukraine
-    </p>
-  </div>
-
-  <div>
-    <h2>Eurasia</h2>
-    <p>
-      Middle East (Arabic)<br />
-      Middle East (English)<br />
-      Morocco<br />
-      Pakistan
-    </p>
-  </div>
-
-  <div>
-    <h2>Asia Pacific</h2>
-    <p>
-      Australia<br />
-      China<br />
-      China - Hong Kong<br />
-      India<br />
-      Indonesia (Bahasa Indonesia)<br />
-      Indonesia (English)<br />
-      Japan<br />
-      Malaysia (English)<br />
-      Malaysia (Malay)<br />
-      New Zealand<br />
-      Singapore<br />
-      South Korea<br />
-      Vietnam (English)<br />
-      Vietnam (Vietnamese)
-    </p>
-  </div>
-</div>`;
+  const worldHtml = Object.entries(world).map(
+    ([continent, countries]) =>
+      `<div><h2>${continent}</h2><p>${countries
+        .map((country) => `${country}<br />`)
+        .join("")}</p></div>`
+  ).join("");
+  newItem.innerHTML = `<div id="conoce-content">${worldHtml}</div>`;
   listItem.parentNode.replaceChild(newItem, listItem);
 
   siguenos.style.borderBottom = "none";
